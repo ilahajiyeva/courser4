c34248b82ae963b498537ef47aac849acd225748
ilahajiyeva <44476352+ilahajiyeva@users.noreply.github.com>
2021-01-08 13:02:29 +0400

    Update hello.js

--- assigments/assigment4-solution/easer/hello.js
+++ assigments/assigment4-solution/easer/hello.js
@@ -1,2 +1,9 @@
-var a = 'Hello Yaakov';
-console.log(a);
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
