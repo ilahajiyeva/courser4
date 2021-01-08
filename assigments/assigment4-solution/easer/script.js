f5a3d6cc386a3d304acd1631b10f3cf664474ed1
ilahajiyeva <44476352+ilahajiyeva@users.noreply.github.com>
2021-01-08 13:01:45 +0400

    Update script.js

--- assigments/assigment4-solution/easer/script.js
+++ assigments/assigment4-solution/easer/script.js
@@ -1,6 +1,16 @@
-var names = ["Paul", "Frank", "Larry", "Paula", "Laura"];
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-    console.log("Hello " + names[i]);
-    console.log("Good Bye " + names[i]);
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
 }
+
+})();
